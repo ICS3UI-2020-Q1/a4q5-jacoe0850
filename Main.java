cde0c12334de679061c7a4382622ceaa22cf8e5d
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-06 07:25:15 +0000

    Finished hailstone

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * takes integer and makes hailstone sequence
+ * @jacoe0850 
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // scanner for input
+    Scanner input = new Scanner(System.in);
+
+    // ask for integer
+    System.out.println("Please enter a positive integer to see the hailstone sequence.");
+    int num = input.nextInt();
+    // start the sequence
+    System.out.println("Hailstone sequence is the following");
+    System.out.println(num);
+    // calculations for sequence
+    while(num > 1){
+      if(num % 2 == 0){
+        num = num/2;
+        System.out.println(num);
+      }else if (num % 2 == 1){
+        num = num*3 + 1;
+        System.out.println(num);
+      }
+      }
+    }
   }
-}
+
